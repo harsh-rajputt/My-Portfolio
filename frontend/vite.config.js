7cbf0d32cfcd39bc85cecc52a33683dc4eebeebd
Harsh raj <harshraj9162192367@gmail.com>
2026-02-01 15:51:20 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:3000/api/v1",
+        target: "http://localhost:8000",
         changeOrigin: true,
         secure: false
       }
